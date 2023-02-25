d3531bd51a37f36f68a481ae19f8c4b26c32eda8
shota_niga <shota.niga@informatix.co.jp>
2023-02-25 13:28:48 +0900

    <add> initial commit

--- 01_AWS/04_ECR/ecr.tf
+++ 01_AWS/04_ECR/ecr.tf
@@ -1,17 +1,9 @@
 ### ECRリポジトリの作成とコンテナイメージのプッシュ
 
-# ローカル変数の定義
-locals {
-  # コンテナイメージ名
-  image_name = "nginx"
-  # Dockerfileがあるディレクトリパス
-  dockerfile_dir = "./src"
-}
-
 # ECRのリポジトリの設定
 resource "aws_ecr_repository" "main" {
   # リポジトリ名
-  name = local.image_name
+  name = var.image_name
   # イメージタグの上書き防止設定(MUTABLE:上書き可/IMMUTABLE:上書き不可)
   image_tag_mutability = "MUTABLE"
   # レジストリにpushしたコンテナイメージのセキュリティスキャン設定
@@ -59,15 +51,15 @@ resource "null_resource" "main" {
   triggers = {
     trigger = "${aws_ecr_repository.main.id}"
   }
-  # 実行コマンド：コンテナイメージのビルドとプッシュ
+  # 実行コマンド：コンテナイメージのビルドとプッシュを行う
   provisioner "local-exec" {
     command = "aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin ${aws_ecr_repository.main.repository_url}"
   }
   provisioner "local-exec" {
-    command = "docker build -t ${local.image_name} ${local.dockerfile_dir}"
+    command = "docker build -t ${var.image_name} ${var.dockerfile_dir}"
   }
   provisioner "local-exec" {
-    command = "docker tag ${local.image_name}:latest ${aws_ecr_repository.main.repository_url}"
+    command = "docker tag ${var.image_name}:latest ${aws_ecr_repository.main.repository_url}"
   }
   provisioner "local-exec" {
     command = "docker push ${aws_ecr_repository.main.repository_url}"
