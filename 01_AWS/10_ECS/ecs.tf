eb73d16e60777b63dbb0929e52999a5c33fb8486
Jingasan <xxx@gmail.com>
2023-09-17 14:44:55 +0900

    <add> initial commit

--- 01_AWS/10_ECS/ecs.tf
+++ 01_AWS/10_ECS/ecs.tf
@@ -287,7 +287,7 @@ resource "aws_security_group_rule" "egress" {
 # ECS ExecによるECSコンテナログインスクリプトの作成
 resource "local_file" "login_ecs_container_script" {
   # 出力先パス
-  filename = "./bastion_script/login_ecs_container.sh"
+  filename = "./script/login_ecs_container.sh"
   # 出力ファイルのパーミッション
   file_permission = "0755"
   # 出力ファイルの内容
@@ -315,7 +315,7 @@ DOC
 # ECSコンテナを開始させるスクリプトの作成
 resource "local_file" "start_ecs_container_script" {
   # 出力先パス
-  filename = "./bastion_script/start_ecs_container.sh"
+  filename = "./script/start_ecs_container.sh"
   # 出力ファイルのパーミッション
   file_permission = "0755"
   # 出力ファイルの内容
@@ -336,7 +336,7 @@ DOC
 # ECSコンテナを停止させるスクリプトの作成
 resource "local_file" "stop_ecs_container_script" {
   # 出力先パス
-  filename = "./bastion_script/stop_ecs_container.sh"
+  filename = "./script/stop_ecs_container.sh"
   # 出力ファイルのパーミッション
   file_permission = "0755"
   # 出力ファイルの内容
