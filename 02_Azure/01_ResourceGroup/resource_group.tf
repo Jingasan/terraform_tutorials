cf8aaf054a3a3ff6b10e489473a3e478987c17f5
Jinga <xxx@gmail.com>
2023-09-18 02:40:27 +0900

    <add> initial commit

---
+++ 02_Azure/01_ResourceGroup/resource_group.tf
@@ -0,0 +1,13 @@
+#============================================================
+# Resource Group
+#============================================================
+resource "azurerm_resource_group" "rg" {
+  # リソースグループ名
+  name = "${var.project_name}-rg"
+  # リージョン
+  location = "japaneast"
+  # タグ
+  tags = {
+    Name = var.project_name
+  }
+}
