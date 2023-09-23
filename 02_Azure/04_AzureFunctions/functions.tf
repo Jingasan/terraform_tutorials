47357e60f6ba3d5cecd82c557c9dacb7f31c1e67
jinga <xxx@gmail.com>
2023-09-23 23:04:29 +0900

    <add> initial commit

--- 02_Azure/04_AzureFunctions/functions.tf
+++ 02_Azure/04_AzureFunctions/functions.tf
@@ -98,7 +98,7 @@ resource "null_resource" "functions_build_upload" {
   }
   # 関数のアップロード
   provisioner "local-exec" {
-    command     = "func azure functionapp publish ${azurerm_linux_function_app.functions.name}"
+    command     = "npx func azure functionapp publish ${azurerm_linux_function_app.functions.name}"
     working_dir = local.func_dir
   }
 }
