96e2fe87b3e0e994e92d2c0ca8f63ba6839ce6d0
jinga <xxx@gmail.com>
2023-09-26 23:39:22 +0900

    <add> initial commit

---
+++ 02_Azure/07_PublicAzureDBPostgreSQL_AzureFunctions/api/src/index.ts
@@ -0,0 +1,72 @@
+import serverlessExpress, {
+  getCurrentInvoke,
+} from "@vendia/serverless-express";
+import express, { Application, Request, Response, NextFunction } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import * as sourceMapSupport from "source-map-support";
+import * as PG from "pg";
+sourceMapSupport.install();
+const app: Application = express();
+// リクエストボディのパース用設定
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// CORS
+app.use(cors());
+
+// DB関連の情報
+const dbHostname = String(process.env.DB_HOSTNAME); // DBサーバーホスト名
+const dbPort = Number(process.env.DB_PORT); // DBサーバーポート番号
+const db = String(process.env.DB_DATABASE); // データベース名
+const dbUsername = String(process.env.DB_USERNAME); // ユーザー名
+const dbPassword = String(process.env.DB_PASSWORD); // パスワード
+
+// DBとの接続初期化
+const initRDSProxyConnection = async (): Promise<PG.Client | false> => {
+  try {
+    console.log("> DB 接続");
+    const pg = new PG.Client({
+      host: dbHostname, // DBサーバーホスト名
+      port: dbPort, // DBサーバーポート番号
+      database: db, // データベース名
+      user: dbUsername, // DBマスターユーザー名
+      password: dbPassword, // DBパスワード
+      ssl: true, // 暗号化通信を有効化
+    });
+    // DBとの接続
+    await pg.connect();
+    return pg;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+};
+
+// GET
+app.get("/api/rds", async (_req: Request, res: Response) => {
+  const console = getCurrentInvoke().event;
+  console.log("DB_HOSTNAME: " + dbHostname);
+  console.log("DB_PORT: " + dbPort);
+  console.log("DB_NAME: " + db);
+  console.log("DB_USERNAME: " + dbUsername);
+  console.log("DB_PASSWORD: " + dbPassword);
+  // DBとの接続初期化
+  const pg = await initRDSProxyConnection();
+  if (!pg) return res.status(503).json("Internal Server Error");
+  // SQLの実行
+  const sql = `show all`;
+  console.log("> SQL 実行：" + sql);
+  const result = await pg.query(sql);
+  return res.status(200).json(result);
+});
+
+// Error 404 Not Found
+app.use((_req: Request, res: Response, _next: NextFunction): any => {
+  return res.status(404).json("Azure Functions is called.");
+});
+
+// 関数のエンドポイント
+const cachedServerlessExpress = serverlessExpress({ app });
+module.exports = async function (event: any, context: any) {
+  return cachedServerlessExpress(event, context, null);
+};
---
+++ 02_Azure/07_PublicAzureDBPostgreSQL_AzureFunctions/variables.tf
@@ -0,0 +1,42 @@
+#============================================================
+# 環境変数の定義（terraform.tfvarsの変数値を受け取る）
+#============================================================
+# プロジェクト名
+variable "project_name" {}
+#============================================================
+# Resource Group
+#============================================================
+# ロケーション
+variable "location" {}
+#============================================================
+# Azure Database for PostgreSQL
+#============================================================
+# PostgreSQLのバージョン
+variable "db_postgres_version" {}
+# コンピューティングサイズ（価格プラン）
+variable "db_sku_name" {}
+# ストレージサイズ (MB) (32GB - 32TB)
+variable "db_storage_mb" {}
+# バックアップ保存期間 (日) (7-35日)
+variable "db_backup_retention_days" {}
+# 管理者ユーザー名
+variable "db_administrator_login" {}
+# パスワード
+variable "db_administrator_password" {}
+# PostgreSQLサーバーへの接続を許可するIPのリスト
+variable "db_firewall_allow_ip_list" {}
+#============================================================
+# Azure Functions
+#============================================================
+# App Serviceの価格プラン (Y1/EP1/EP2/EP3/B1/B2/B3/S1/S2/S3/P1v2/P2v2/P3v2)
+# https://azure.microsoft.com/ja-jp/pricing/details/app-service/linux/
+variable "functions_sku_name" {}
+# Azure FunctionsのNodeランタイムのバージョン
+variable "functions_node_version" {}
+#============================================================
+# Blob Storage (Azure Functions用)
+#============================================================
+# 価格プラン (Standard/Premium)
+variable "storage_account_tier" {}
+# 冗長性 (LRS/GRS/RAGRS/ZRS/GZRS/RAGZRS)
+variable "storage_account_replication_type" {}
