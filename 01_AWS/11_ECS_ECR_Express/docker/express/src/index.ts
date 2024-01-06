e017d3c1776bdc7478b4e90992b21a089a974f43
jinga <xxx@gmail.com>
2024-01-06 15:45:16 +0900

    <add> initial commit

--- 01_AWS/11_ECS_ECR_Express/docker/express/src/index.ts
+++ 01_AWS/11_ECS_ECR_Express/docker/express/src/index.ts
@@ -1,11 +1,13 @@
-import express, { Application, Request, Response, NextFunction } from "express";
+import express from "express";
 import cors from "cors";
-const app: Application = express();
+const app = express();
 const PORT = 80;
-
-// リクエストボディのパース用設定
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// リクエストボディのパース設定
+// 1. JSON形式のリクエストボディをパースできるように設定
+// 2. フォームの内容をパースできるように設定
+// 3. Payload Too Large エラー対策：50MBまでのデータをPOSTできるように設定(default:100kb)
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // CORS
 const corsOptions = {
@@ -19,50 +21,31 @@ const corsOptions = {
 app.use(express.static("public"));
 
 // GET
-app.get(
-  "/users/:id",
-  cors(corsOptions),
-  async (req: Request, res: Response, _next: NextFunction) => {
-    return res.status(200).json({ Query: req.query });
-  }
-);
+app.get("/users/:id", cors(corsOptions), async (req, res) => {
+  return res.status(200).json({ Query: req.query });
+});
 
 // POST
-app.post(
-  "/users/:id",
-  cors(corsOptions),
-  async (req: Request, res: Response, _next: NextFunction) => {
-    return res.status(200).json({ PostBody: req.body });
-  }
-);
+app.post("/users/:id", cors(corsOptions), async (req, res) => {
+  return res.status(200).json({ PostBody: req.body });
+});
 
 // PUT
-app.put(
-  "/users/:id",
-  cors(corsOptions),
-  async (req: Request, res: Response, _next: NextFunction) => {
-    return res.status(200).json({ RequestHeader: req.headers });
-  }
-);
+app.put("/users/:id", cors(corsOptions), async (req, res) => {
+  return res.status(200).json({ RequestHeader: req.headers });
+});
 
 // DELETE
-app.delete(
-  "/users/:id",
-  cors(corsOptions),
-  async (req: Request, res: Response, _next: NextFunction) => {
-    return res.status(200).json({ URLParams: req.params.id });
-  }
-);
+app.delete("/users/:id", cors(corsOptions), async (req, res) => {
+  return res.status(200).json({ URLParams: req.params.id });
+});
 
 // Error 404 Not Found
-app.use(
-  cors(corsOptions),
-  async (_req: Request, res: Response, _next: NextFunction) => {
-    return res.status(404).json({
-      error: "Not Found",
-    });
-  }
-);
+app.use(cors(corsOptions), async (_req, res) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
 
 // サーバーを起動する処理
 try {
