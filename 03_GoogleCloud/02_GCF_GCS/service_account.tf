bfde740452d23c6401a1ea389409f377073c1977
jinga <xxx@gmail.com>
2024-01-12 11:28:45 +0900

    <add> initial commit

--- 03_GoogleCloud/02_GCF_GCS/service_account.tf
+++ 03_GoogleCloud/02_GCF_GCS/service_account.tf
@@ -2,19 +2,20 @@
 # Service Account
 #============================================================
 
-# サービスアカウントの作成
-# GCSでPresignedURLを発行するためのサービスアカウントを作成する
+# GCFからGCSを操作するためのサービスアカウントの作成
 resource "google_service_account" "gcs" {
+  depends_on = [google_project_service.apis]
   # サービスアカウントID
-  account_id = "gcs-presignedurl"
+  account_id = "gcs-service-account"
   # サービスアカウントの名前
-  display_name = "GCS PresignedURL"
+  display_name = "Service Account for GCS"
   # サービスアカウントの説明文
-  description = "Service Account for publishing GCS PresignedURL"
+  description = "Service Account for GCS"
 }
 
 # サービスアカウントに権限(ロール)を割り当て
 resource "google_project_iam_member" "gcs" {
+  depends_on = [google_project_service.apis]
   # プロジェクトID
   project = var.project_id
   # ロール割り当て先のユーザー：サービスアカウントを指定
@@ -25,6 +26,7 @@ resource "google_project_iam_member" "gcs" {
 
 # アカウントキーの作成
 resource "google_service_account_key" "key" {
+  depends_on = [google_project_service.apis]
   # アカウントキーを作成するサービスアカウントのID
   service_account_id = google_service_account.gcs.name
 }
