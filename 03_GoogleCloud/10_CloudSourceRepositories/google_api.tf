e3f4ec9bbd57ee604bed88f276ee1101b7a4372f
jinga <xxx@gmail.com>
2024-01-30 10:20:14 +0900

    <add> initial commit

---
+++ 03_GoogleCloud/10_CloudSourceRepositories/google_api.tf
@@ -0,0 +1,22 @@
+#============================================================
+# Google Cloud APIの有効化
+#============================================================
+
+# 有効化するAPIの一覧
+locals {
+  services = {
+    "Cloud Source Repositories API" : "sourcerepo.googleapis.com",
+  }
+}
+
+# APIの有効化
+resource "google_project_service" "apis" {
+  # APIを有効化する対象のプロジェクトID
+  project = var.project_id
+  # 有効化するAPI群
+  for_each = local.services
+  service  = each.value
+  # terraform destroy時にAPIを無効化する
+  disable_dependent_services = true
+  disable_on_destroy         = true
+}
--- 03_GoogleCloud/10_CloudSourceRepositories/repository.tf
+++ 03_GoogleCloud/10_CloudSourceRepositories/repository.tf
@@ -4,6 +4,7 @@
 
 # リポジトリの作成
 resource "google_sourcerepo_repository" "repo" {
+  depends_on = [google_project_service.apis]
   # リポジトリ名
   name = var.project_id
 }
